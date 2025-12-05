c4f5f1ac640330485f1a53799ca77b94acd7dae5
deevashb24 <deevash2006@gmail.com>
2025-12-05 18:46:48 +0530

    chore: add components stubs + tsconfig alias + lucide-react

---
+++ components/dashboard-navbar.tsx
@@ -0,0 +1,4 @@
+"use client";
+import React from "react";
+export function DashboardNavbar(){ return <header className="flex justify-between p-4">ResumeAI</header>; }
+export default DashboardNavbar;
